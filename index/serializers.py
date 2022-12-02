4819d2153232ccb52d88e67a6b138201cac82872
Olorunfemi Oloko <f3mioloko@gmail.com>
2022-12-02 15:01:03 +0100

    Revert "ok"
    This reverts commit 1bf6a239be533fa4715b3c74bf02518c97fe77e5.

--- index/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers 
-from .models import *
-
-class s_and_t_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = s_and_t, Order, OrderItem, Customer
-        field = ('__all__')
--- userz/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserzConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userz'
